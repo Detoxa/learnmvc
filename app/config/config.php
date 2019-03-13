842eb385355de2af6d84f82200188f3f04b8f382
Vit Safarik <vitsaf@gmail.com>
2019-03-13 21:11:35 +0100

    create PDO

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,11 @@
 <?php
+    // DB Params
+    define('DB_HOST', '172.16.238.12');
+    define('DB_USER', 'root');
+    define('DB_PASS' , '');
+    define('DB_NAME', 'learnmvc');
+
+
     // App root
     define('APPROOT' , dirname(dirname(__FILE__)));
     // URL Root
